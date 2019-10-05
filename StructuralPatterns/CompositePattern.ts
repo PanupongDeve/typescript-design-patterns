59bd50ef57c1b394893edc2e9fecdc235fe20779
Panupong <5635512110@psu.ac.th>
2019-10-06 04:31:12 +0700

    update composition pattern

--- StructuralPatterns/CompositePattern.ts
+++ StructuralPatterns/CompositePattern.ts
@@ -20,7 +20,7 @@ class Employee {
         this.subordinates = subordinatesRemoved;
     }
 
-    public getSuboardinates() {
+    public getSuboardinates(): Employee[] {
         return this.subordinates;
     }
 
@@ -31,4 +31,38 @@ class Employee {
 
 const CEO: Employee = new Employee("John", "CEO", 30000);
 
+const headSales: Employee = new Employee("Robert", "Head Sales", 20000);
+
+const headMarketing: Employee = new Employee("Michel", "Head Marketing", 20000);
+
+const clerk1: Employee = new Employee("Laura", "Marketing", 10000);
+const clerk2: Employee = new Employee("Bob", "Marketing", 10000);
+
+const salesExcutive1: Employee = new Employee("Richard", "Sales", 10000);
+const salesExcutive2: Employee = new Employee("Rob", "Sales", 10000);
+
+CEO.add(headSales);
+CEO.add(headMarketing);
+
+
+headSales.add(salesExcutive1);
+headSales.add(salesExcutive2);
+
+headMarketing.add(clerk1);
+headMarketing.add(clerk2);
+
+
 console.log(CEO.toString());
+
+const headEmployee: Employee[] = CEO.getSuboardinates();
+
+
+headEmployee.forEach(he => {
+    console.log(he.toString());
+    const employee = he.getSuboardinates();
+
+    employee.forEach(e => {
+        console.log(e.toString());
+    });
+
+});
